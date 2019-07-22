332cf5adcbfabed87a9e102ef91fe82031a2d93c
AkshayGulhane46 <argulhane10246@gmail.com>
2019-07-22 17:43:42 +0530

    first commitsjf.py

---
+++ new/lambda.py
@@ -0,0 +1,18 @@
+#what is lambda
+#a function which has no name , is known as lambda function
+#this is also known as anonymous function
+
+#syntax
+#when there is no need of resuability then use lambda
+# def calc(x):
+#     y=x*2
+#     print(y)
+#
+# # calc(10)
+#
+# calc=lambda x:x*2
+# print(calc(10))
+
+z=lambda a,b,c:a+b+c
+print(z(3,2,5))
+
